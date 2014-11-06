e04d9da9082e3cf6bea62b9b13544a0de7ac46a5
LubyRuffy <LubyRuffy@mail.com>
2014-11-06 20:50:05 +0800

    add cdn ip check

--- watch_log_apache.rb
+++ watch_log_apache.rb
@@ -10,7 +10,7 @@ class LogLine
     @line_parts = []
     parse_origin_line
   end
-  
+
   def parse_origin_line
     return if @origin_line.nil? or @origin_line.empty?
     quoted_word = ""
@@ -31,12 +31,12 @@ class LogLine
       end
     } 
   end
-  
+
   def [](idx)
     return nil if idx < 0 or idx >= size
     return @line_parts[idx]
   end
-  
+
   def size
     return @line_parts.size
   end
@@ -52,17 +52,33 @@ IP_CONFIG_FILE = '/usr/local/apache/conf/fofa_deny.conf'
 LOG_REGEX = "/usr/local/apache/domlogs/*"
 $domains = []
 `grep -E "ServerName|ServerAlias" /usr/local/apache/conf/httpd.conf`.each_line{|line|
-      line.chomp.split(/[ \t\r\n]/).each{|r|
-		unless r.include?("ServerName") || r.include?("ServerAlias")
-		$domains << r.chomp if r.length>3
-		$domains = $domains.uniq
-		end
-	}
+  line.chomp.split(/[ \t\r\n]/).each{|r|
+    unless r.include?("ServerName") || r.include?("ServerAlias")
+      $domains << r.chomp if r.length>3
+      $domains = $domains.uniq
+    end
+  }
 }
 #puts $domains
 $blackips = []
 $blackrefers = []
 $begintime = ARGV[0]
+
+require "ipaddr" 
+def not_cdn_ip(ip) 
+  $whith_range ||= [] 
+  $white_ip_range ||= %w|199.27.128.0/21 173.245.48.0/20 103.21.244.0/22 103.22.200.0/22 103.31.4.0/22 141.101.64.0/18 108.162.192.0/18 190.93.240.0/20 188.114.96.0/20 197.234.240.0/22 198.41.128.0/17 162.158.0.0/15 104.16.0.0/12 199.83.128.0/21 198.143.32.0/19 149.126.72.0/21 103.28.248.0/22 45.64.64.0/22 185.11.124.0/22 192.230.64.0/18| 
+  unless $whith_range.size>0 
+    $white_ip_range.each{|iprange| 
+      $whith_range << IPAddr.new(iprange) 
+    } 
+  end 
+  $whith_range.each{|net| 
+    return false if net===ip 
+  } 
+  true 
+end 
+
 #puts $begintime
 def check_line(line)
   need_delete = false
@@ -91,37 +107,43 @@ def check_line(line)
     end
   elsif (uri.include?('.css') || uri.include?('.jpg') || uri.include?('.js'))&& refer.size>5
     unless $domains.detect{|d| refer.include?(d)}
-    u = URI(refer.rchomp('"').chomp('"'))
-    irefer = u.host+u.path
-    unless $blackrefers.include?(irefer)
-      $blackrefers << irefer
-      puts "Found bad host: #{irefer} => #{uri}"
-      if u.host.downcase.include?('google') && (u.path.size<3 || u.path.include?('blank.html'))
-        need_delete = true
-      else
-        cmd="curl '#{irefer}?key=fkmirrors'"
-        puts cmd
-        `#{cmd}`
+      u = URI(refer.rchomp('"').chomp('"'))
+      irefer = u.host+u.path
+      unless $blackrefers.include?(irefer)
+        $blackrefers << irefer
+        puts "Found bad host: #{irefer} => #{uri}"
+        if u.host.downcase.include?('google') && (u.path.size<3 || u.path.include?('blank.html'))
+          need_delete = true
+        else
+          cmd="curl '#{irefer}?key=fkmirrors'"
+          puts cmd
+          `#{cmd}`
+        end
       end
-    end
-    msg = 'unknown host use css'
+      msg = 'unknown host use css'
     end
 
   end
   if need_delete && !$blackips.include?(ip)
     if ip.size>7
-    unless `grep 'deny from #{ip}' #{IP_CONFIG_FILE}`.include?("deny from #{ip}")
-    $blackips << ip
-    puts '+'*40
-    puts Time.now.to_s
-    puts "block ip #{ip}, reson: #{msg}"
-    puts line
-    puts '-'*40
-    
-    #`iptables -I INPUT -s #{ip} -j DROP`
-    cmd = "sed '$!N;$!P;$!D;s/\\(\\n\\)/&deny from #{ip}\\1/;' #{IP_CONFIG_FILE} \> tmp.conf; /bin/cp tmp.conf #{IP_CONFIG_FILE}; /etc/init.d/httpd restart"
-    `#{cmd}`
-    end 
+      unless not_cdn_ip(ip) 
+        puts "[WARNING] found CDN ip!" 
+        puts line 
+        return 
+      end 
+
+      unless `grep 'deny from #{ip}' #{IP_CONFIG_FILE}`.include?("deny from #{ip}")
+        $blackips << ip
+        puts '+'*40
+        puts Time.now.to_s
+        puts "block ip #{ip}, reson: #{msg}"
+        puts line
+        puts '-'*40
+
+        #`iptables -I INPUT -s #{ip} -j DROP`
+        cmd = "sed '$!N;$!P;$!D;s/\\(\\n\\)/&deny from #{ip}\\1/;' #{IP_CONFIG_FILE} \> tmp.conf; /bin/cp tmp.conf #{IP_CONFIG_FILE}; /etc/init.d/httpd restart"
+        `#{cmd}`
+      end 
     end
   end
 rescue => e
@@ -133,12 +155,12 @@ if !$begintime
   t.tail(LOG_REGEX)
   t.subscribe do |path, line|
     unless path.include?('bytes_lo')
-	#puts path, line
-        check_line(line)
+      #puts path, line
+      check_line(line)
     end
   end
 elsif ARGV[1]
-    check_line(ARGV[0])
+  check_line(ARGV[0])
 else
   File.open("/www/wdlinux/nginx/logs/access.log") do |file| 
     process = false
